34563b65c70d6190c767216356ea9181846a33ef
sandesh63 <62508295+sandesh63@users.noreply.github.com>
2023-12-29 08:48:47 +0530

    Update ExspenseDataDTO.java

--- src/main/java/com/exspensetracker/dto/ExspenseDataDTO.java
+++ src/main/java/com/exspensetracker/dto/ExspenseDataDTO.java
@@ -6,6 +6,9 @@ public class ExspenseDataDTO  {
 private int id;
 private String category;
 private Date date;
+	private String type;
+	private double inHandBalance;
+private double bankBalance;
 private double amount;
 private String work;
 private String receiver;
@@ -36,6 +39,24 @@ public void setAmount(double amount) {
 }
 public String getWork() {
 	return work;
+}
+	public double getInHandBalance() {
+	return inHandBalance;
+}
+public String getType() {
+	return type;
+}
+public void setType(String type) {
+	this.type = type;
+}
+public void setInHandBalance(double inHandBalance) {
+	this.inHandBalance = inHandBalance;
+}
+public double getBankBalance() {
+	return bankBalance;
+}
+public void setBankBalance(double bankBalance) {
+	this.bankBalance = bankBalance;
 }
 public void setWork(String work) {
 	this.work = work;
